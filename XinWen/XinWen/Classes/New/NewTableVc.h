25acb1e45276cbdd4f8b90a7b76858ee8d44d7f0
your name <861144636@qq.com>
2016-01-19 11:41:43 +0800

    搭建 页面

---
+++ XinWen/XinWen/Classes/New/NewTableVc.h
@@ -0,0 +1,13 @@
+//
+//  NewTableVc.h
+//  XinWen
+//
+//  Created by fei-ke on 16/1/19.
+//  Copyright © 2016年 fei-ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTableVc : UITableViewController
+
+@end
