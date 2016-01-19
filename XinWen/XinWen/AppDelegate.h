337b9c8fc44e4415a66ef4f9160a5ced898694d0
your name <861144636@qq.com>
2016-01-19 10:39:28 +0800

    初始化 页面

---
+++ XinWen/XinWen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XinWen
+//
+//  Created by fei-ke on 16/1/18.
+//  Copyright © 2016年 fei-ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XinWen/XinWen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XinWen
+//
+//  Created by fei-ke on 16/1/18.
+//  Copyright © 2016年 fei-ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
